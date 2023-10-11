8e9b784a15901e22df1c487de117047e50548c0b
itaybroder <itay@intune.ltd>
2023-10-11 15:44:47 +0300

    Fixed stuff

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -10,20 +10,22 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   const isMobile = useMediaQuery({ query: '(max-width: 1000px)' })
+  const isMiddle = useMediaQuery({ query: '(max-width: 640px)' })
   return (
     <main>
       <div className='h-[9%] fixed w-full top-0 z-10'>
-        <Navbar />
+        <Navbar
+        isMobile={isMobile}
+        />
       </div>
       {isMobile ? 
         <>
-          <div className='h-[calc(91% - 9%)] w-full pt-[9%]'>
-            <MobileSidebar />
+          <div className={`h-[calc(91% - 9%)] w-full ${isMiddle ? 'pt-[19%]' : 'pt-[10%]'}`}>
             {children}
           </div>
         </>
       :
-        <div className='h-[90%] flex flex-row pt-[9%]'> 
+        <div className='h-[90%] flex flex-row pt-20'> 
           <div className='w-[20%] h-full fixed left-0 top-[9%] z-10'>
             <Sidebar />
           </div>
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -7,8 +7,8 @@ import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "../ui/button";
 import LoginDialog from "../loginModal";
 import { signOut, useSession } from "next-auth/react";
-
-const Nav = () => {
+import MobileSideBar from "@/components/layout/MobileSidebar";
+const Nav = ({isMobile}: {isMobile: boolean}) => {
   const router = useRouter();
   const { data: session } = useSession();
   return (
@@ -17,14 +17,16 @@ const Nav = () => {
             <div className="flex w-full flex-row items-center gap-2">
                 <Image src='/logo.png' alt='logo' width={1024} height={1024} className="h-10 w-10 rounded" />
                 <h2 className="text-xl font-mono">IronTruth</h2>
+                {isMobile && (<MobileSideBar/>)}
             </div>
+            
             <div className="flex justify-end gap-3 w-full" >
               <div className="flex flex-row gap-2 items-center">
                   {
                   session?.user ? (
                       <>
-                        <UserButton user={session.user} />
                         <ModeToggle />
+                        <UserButton user={session.user} />
                       </>
                   ):(
                     <>
